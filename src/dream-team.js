2c31f9a8140b1c28084f24ca5f7537179e72e1e8
Gavr1k <47858531+Gavr1k@users.noreply.github.com>
2021-03-08 06:16:06 +0300

    feat: add task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  return_str = '';
+    if(members === null || members === undefined)
+    {
+        return false;
+    }
+    if(members.constructor.name != "Array")
+    {
+        return false;
+    }
+    for(let i = 0; i < members.length; i++)
+    {
+        if(typeof members[i] != 'string')
+        {
+            continue;
+        }
+        return_str += ((members[i].replace(/\s+/g, '')).slice(0,1)).toUpperCase();
+    }
+    return String(((return_str.split('')).sort()).join(''));
 };
