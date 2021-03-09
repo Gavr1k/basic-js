fcb643e371705f520de8357c78254bf869f8a89c
Gavr1k <47858531+Gavr1k@users.noreply.github.com>
2021-03-09 04:04:25 +0300

    Delete hanoi-tower.js

--- src/hanoi-tower.js
+++
@@ -1,7 +0,0 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-  turns = 2**disksNumber - 1;
-    seconds = Math.floor(turns/turnsSpeed*60*60);
-    return { turns: turns, seconds: seconds };
-};
